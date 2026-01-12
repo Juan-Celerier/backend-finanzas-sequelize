5ce78038826fb37dc4d5423c67eae51fc68b94b1
Juan Celerier <juancelerier15@gmail.com>
2026-01-11 22:17:56 -0300

    Agregar un salto de línea en el archivo de configuración de la base de datos

--- config/database.ts
+++ config/database.ts
@@ -5,6 +5,7 @@ if (!process.env.DATABASE_URL) {
   throw new Error("DATABASE_URL environment variable is required");
 }
 
+
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
   logging: false,
